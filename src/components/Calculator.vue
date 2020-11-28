09cc5314aea77258ce3f9aa58489cf91f3467876
Başar Özcan <basarozcan@gmail.com>
2020-11-28 19:02:11 +0300

    change api and some cosmetic changes

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -8,36 +8,59 @@
         <b-field label="Price">
           <b-input v-model="price" @keyup.enter.native="calculate"></b-input>
         </b-field>
-        <a v-if="isLoading" class="button is-warning" @click="calculate">Hesapla</a>
-        <a v-else class="button is-primary" @click="calculate">Hesaplanıyor...</a>
+        <a v-if="isLoading" class="button is-warning" @click="calculate">Calculate</a>
+        <a v-else class="button is-primary" @click="calculate">Calculating...</a>
       </section>
       <div class="result"></div>
     </div>
     <div class="logs">
-      <div class="columns is-multiline is-mobile">
-        <div class="column is-half" v-for="item in items.slice().reverse()">
-          <div class="box">
-            <article class="media">
-              <div class="media-content">
-                <div class="content">
-                  <p>
-                    <strong>{{ item.number }}</strong>
-                    <small>{{ item.name }}</small>
-                    <br>
-                    <small>
-                      {{ item.price }} Ödendi -
-                      {{ item.income }} Kazanılacak
-                    </small>
-                    <br>
-                    <strong>x{{ item.incomex }}</strong>
-                    <small> +{{ round((item.income - item.price),100) }} TL</small>
-                  </p>
-                </div>
-              </div>
-            </article>
-          </div>
-        </div>
-      </div>
+      <b-table 
+      :data="items">
+        <b-table-column field="image" label="Image" width="40" v-slot="props">
+          <article class="media">
+            <figure class="media-left">
+                <p class="image is-64x64">
+                  <img :src="props.row.setImageUrl" :alt="props.row.number">
+                </p>
+            </figure>
+        </article>
+        </b-table-column>
+        <b-table-column field="number" label="Set No" width="40" sortable v-slot="props">
+          {{ props.row.number }}
+        </b-table-column>
+        <b-table-column field="name" label="Set Name" width="40" sortable v-slot="props">
+          {{ props.row.name }}
+        </b-table-column>
+        <b-table-column field="minifiguresCount" label="Minifigures" width="40" sortable numeric v-slot="props">
+          {{ props.row.minifiguresCount }}
+        </b-table-column>
+        <b-table-column field="partsCount" label="Pieces" width="40" sortable numeric v-slot="props">
+          {{ props.row.partsCount }}
+        </b-table-column>
+        <b-table-column field="price" label="Paid Price" width="40" sortable numeric v-slot="props">
+          {{ props.row.price }}
+        </b-table-column>
+        <b-table-column field="income" label="PO Income" width="40" sortable numeric v-slot="props">
+          {{ props.row.income }}
+        </b-table-column>
+        <b-table-column field="incomex" label="Factor" width="40" sortable numeric v-slot="props">
+          <b>{{ props.row.incomex }}</b>
+        </b-table-column>
+        <b-table-column field="actions" label="Actions" width="40" numeric v-slot="props">
+          <a target="_blank" :href="props.row.catalogUri">
+            <b-icon
+                pack="fas"
+                icon="money-bill-alt">
+            </b-icon>
+          </a>
+          <a target="_blank" :href="props.row.inventeroUri">
+            <b-icon
+                pack="fas"
+                icon="puzzle-piece">
+            </b-icon>
+          </a>
+        </b-table-column>
+      </b-table>
     </div>
   </div>
 </template>
@@ -54,7 +77,12 @@ export default {
         price: null,
         name: null,
         income: null,
-        incomex: null
+        incomex: null,
+        catalogUri: null,
+        inventeroUri: null,
+        setImageUrl: null,
+        minifiguresCount: null,
+        partsCount: null
       },
       items: []
     };
@@ -64,25 +92,28 @@ export default {
       return Math.round(value * round) / round;
     },
     retrieve: async function() {
+
       try {
         await this.axios
           .get(
-            "http://basarozcan.com/brickton/calc/newapi.php?set=" +
+            "https://brickton-api.netlify.app/.netlify/functions/api/po/" +
               this.number +
-              "&price=" +
-              this.price +
-              "&currency=TRY"
+              "/" +
+              this.price
           )
           .then(getResponse => {
+            const data = getResponse.data[0]
             this.items.push({
               number: this.number,
               price: this.round(this.price, 100),
-              name: getResponse.data.data.title,
-              income: this.round(getResponse.data.try.sixmavg, 100),
-              incomex: this.round(
-                getResponse.data.try.sixmavg / this.price,
-                1000
-              )
+              name: data.setName,
+              income: this.round(data.try.sixMonth, 100),
+              incomex: data.calculation.sixMonthFactor,
+              catalogUri: data.bricklink.catalogUri,
+              inventeroUri: data.bricklink.inventeroUri,
+              setImageUrl: data.brickset.set.set_img_url,
+              minifiguresCount: data.brickset.minifigures.count,
+              partsCount: data.brickset.set.num_parts
             });
             this.isLoading = true;
             this.number = null;
@@ -91,7 +122,6 @@ export default {
           .catch(function(error) {
             console.log("HATA " + error);
           });
-        //http://basarozcan.com/brickton/calc/mini.php?set=75040&price=100&currency=TRY
       } catch (error) {
         alert(error);
       }
